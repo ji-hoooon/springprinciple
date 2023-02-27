c108deb48a2db17b612942da3f938790c12abcc9
ji-hoooon <ljh6384@naver.com>
2023-02-27 17:09:38 +0900

    ADD MetaAnnotation

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,11 +3,31 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+@Retention(RetentionPolicy.RUNTIME)
 
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+//UnitTest는 메타어노테이션으로는 사용 불가
+//@Target(ElementType.METHOD)
+//UnitTest는 메타어노테이션으로 사용하기 위해서 타겟을추가
+@Target({ElementType.ANNOTATION_TYPE,ElementType.METHOD})
+@Test
+@interface UnitTest{
+}
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
+//    @UnitTest
     void simpleHelloService(){
         SimpleHelloService helloService = new SimpleHelloService();
 
