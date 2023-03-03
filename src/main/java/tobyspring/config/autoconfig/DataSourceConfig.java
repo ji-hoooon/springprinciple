08ac80f5704c6a7d132bf711037ba788acb5ff5b
ji-hoooon <ljh6384@naver.com>
2023-03-03 21:19:47 +0900

    ADD DataSourceConfig to JdbcTemplate, JdbcTransactionManager, @EnableTransactionManageMent (+ @HellobootTest)

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -2,10 +2,14 @@ package tobyspring.config.autoconfig;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
@@ -21,7 +25,9 @@ import java.sql.Driver;
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 //자동 구성 클래스가 @Conditional 조건이 충족할 때만 프로퍼티 파일이 빈으로 등록될 수 있도록 하는 어노테이션
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
-
+//@Import를 메타 어노테이션하는 어노테이션으로
+//구성정보를 가진 클래스를 임포트하는 역할 수행
+@EnableTransactionManagement
 public class DataSourceConfig {
     //Hikari 관련 클래스 존재할 때만 사용하도록 변경
     @Bean
@@ -64,5 +70,31 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+    @Bean
+    //파라미터로 데이터 소스를 받는다.
+    //커스텀 빈을 만들 경우를 위해 해당 빈을 못찾을 경우도 추가해둔다.
+    @ConditionalOnMissingBean
+    //스프링이 제공하는 어노테이션으로
+    //해당 빈 메서드 실행시,
+    // 스프링 컨테이너의 빈 구성정보 파라미터로 전달한 타입의 빈이 한개만 등록되어있을 경우 해당 빈을 가져와서 사용한다.
+    @ConditionalOnSingleCandidate(DataSource.class)
+    JdbcTemplate jdbcTemplate(DataSource dataSource){
+        //복잡한 기능을 위해서는 프로퍼티 클래스를 추가해 설정해도 되지만, 일반적으로는 오브젝트 만드는 것으로 충분
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    //파라미터로 데이터 소스를 받는다.
+    //커스텀 빈을 만들 경우를 위해 해당 빈을 못찾을 경우도 추가해둔다.
+    @ConditionalOnMissingBean
+    //스프링이 제공하는 어노테이션으로
+    //해당 빈 메서드 실행시,
+    // 스프링 컨테이너의 빈 구성정보 파라미터로 전달한 타입의 빈이 한개만 등록되어있을 경우 해당 빈을 가져와서 사용한다.
+    @ConditionalOnSingleCandidate(DataSource.class)
+    //일반적으로 @Transactional이라는 어노테이션을 이용해서 선언적인 방법으로 사용하는 빈으로,
+    //직접 사용할 경우 PlatformTransactionManager라는 인터페이스로 주입해서 사용한다.
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource){
+        //복잡한 기능을 위해서는 프로퍼티 클래스를 추가해 설정해도 되지만, 일반적으로는 오브젝트 만드는 것으로 충분
+        return new JdbcTransactionManager(dataSource);
+    }
 
 }
--- src/test/java/tobyspring/helloboot/DataSourceTest.java
+++ src/test/java/tobyspring/helloboot/DataSourceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -14,13 +15,18 @@ import java.sql.SQLException;
 //단위 테스트가 아닌
 //스프링 컨테이너를 띄워서 빈 구성정보를 넣고, 빈을 가져와서 테스트
 
-//JUnit5을 확장하는 어노테이션
-@ExtendWith(SpringExtension.class)
-//빈들을 로딩할 정보를 가져오는 어노테이션 추가 모든 빈 구성정보를 읽어오도록 run메서드를 가진 클래스 전달
-@ContextConfiguration(classes = HellobootApplication.class)
-//외부설정을 위한 자동구성편에서도 얘기했듯, application.properties를 이용한 등록은
-//스프링 부트 초기화 과정에서 추가해줘야한다.
-@TestPropertySource("classpath:/application.properties")
+////JUnit5을 확장하는 어노테이션
+//@ExtendWith(SpringExtension.class)
+////빈들을 로딩할 정보를 가져오는 어노테이션 추가 모든 빈 구성정보를 읽어오도록 run메서드를 가진 클래스 전달
+//@ContextConfiguration(classes = HellobootApplication.class)
+////외부설정을 위한 자동구성편에서도 얘기했듯, application.properties를 이용한 등록은
+////스프링 부트 초기화 과정에서 추가해줘야한다.
+//@TestPropertySource("classpath:/application.properties")
+//-> 어노테이션이 너무 많으므로, 하나의 어노테이션으로 합성한다.
+@HellobootTest
+
+//테스트가 독립적으로 수행되게 하기 위해서 클래스 전체를 트랜잭션으로
+@Transactional
 public class DataSourceTest {
     //필드 주입을 위한 어노테이션
     @Autowired
